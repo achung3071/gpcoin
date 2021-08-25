4eacc85403e1bd58df929b7d393221cc664adae8
Andrew Chung <drew.c3071@gmail.com>
2021-08-25 21:35:04 +0900

    create basic blockchain

--- main.go
+++ main.go
@@ -11,11 +11,38 @@ type block struct {
 	prevHash string
 }
 
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) == 0 {
+		return ""
+	}
+	return b.blocks[len(b.blocks)-1].hash
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	// hash data with prev hash  using SHA256 - need to convert string to []byte
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash) // hex number as hash
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev hash: %s\n\n", block.prevHash)
+	}
+}
+
 // Main function
 func main() {
-	genesisBlock := block{"First block", "", ""}
-	// hash data with prev hash (currently non-existent) using SHA256
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	genesisBlock.hash = fmt.Sprintf("%x", hash) // hex number as hash
-	fmt.Println(genesisBlock)
+	b := blockchain{}
+	b.addBlock("First block")
+	b.addBlock("Second block")
+	b.addBlock("Third block")
+	b.listBlocks()
 }
