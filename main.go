851637e5ebb9475d9a812a85b5ecc25c2140f4d9
Andrew Chung <drew.c3071@gmail.com>
2021-08-25 21:20:26 +0900

    create genesis block

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
 
 // Main function
 func main() {
-	fmt.Println("Welcome!")
+	genesisBlock := block{"First block", "", ""}
+	// hash data with prev hash (currently non-existent) using SHA256
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	genesisBlock.hash = fmt.Sprintf("%x", hash) // hex number as hash
+	fmt.Println(genesisBlock)
 }
