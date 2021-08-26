1b4ef9c17259c6af90c67919e8f8b0d4f2ea57be
Andrew Chung <drew.c3071@gmail.com>
2021-08-26 21:03:00 +0900

    implement TextMarshaler to add localhost to urls

--- main.go
+++ main.go
@@ -9,12 +9,31 @@ import (
 
 const port string = ":5000"
 
+type URL string // custom type
+
+// This is a built-in interface in the "encoding" package with
+// a method that the json encoder uses to encode text. We can have
+// the URL type implement this interface to change how it is encoded.
+/* type TextMarshaler interface {
+	MarshalText() (text []byte, err error)
+} */
+func (u URL) MarshalText() ([]byte, error) {
+	fullUrl := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(fullUrl), nil
+}
+
 type URLDescription struct {
-	URL         string `json:"url"` // struct field tag -> renames based on encoding
+	URL         URL    `json:"url"` // struct field tag -> renames based on encoding
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"` // omits this field when non-existent
 }
+
+// Another common interface: Stringer interface (implements String())
+// "fmt" uses String() to display Go structs/instances when printed
+/* func (u URLDescription) String() string {
+	return "This is a url description"
+} */
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	urls := []URLDescription{
