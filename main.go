19b1fb69d16df0ee7bdd7ebe20f68641afb892a1
Andrew Chung <drew.c3071@gmail.com>
2021-08-27 16:08:49 +0900

    use flagset to specify port as flag in cli app

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/achung3071/gpcoin/api"
-	"github.com/achung3071/gpcoin/webapp"
 )
 
+func instructions() {
+	fmt.Printf("This is the GPCoin CLI.\n\n")
+	fmt.Printf("Use one of the following commands:\n\n")
+	fmt.Println("web:	Run the HTML web application")
+	fmt.Println("api:	Run the REST API server")
+	os.Exit(0) // no error; Exit(1) is an eror
+}
+
 func main() {
-	go webapp.Start(4000)
-	api.Start(5000)
+	if len(os.Args) < 2 {
+		instructions()
+	}
+
+	// can specify set of flags & how to handle flag errors
+	apiFlagSet := flag.NewFlagSet("api", flag.ExitOnError)
+	portFlag := apiFlagSet.Int("port", 5000, "Sets the port of the REST API server")
+
+	switch os.Args[1] {
+	case "web":
+		fmt.Println("Starting web app...")
+	case "api":
+		apiFlagSet.Parse(os.Args[2:]) // parse flags from 2nd arg onwards
+	default:
+		instructions()
+	}
+
+	if apiFlagSet.Parsed() {
+		api.Start(*portFlag)
+	}
 }
