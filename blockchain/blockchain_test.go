bd309fa0ac910ef210d5500e3959efec1425b23d
Andrew Chung <drew.c3071@gmail.com>
2021-09-06 23:11:45 +0900

    test Blockchain() singleton accessor

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"sync"
+	"testing"
+
+	"github.com/achung3071/gpcoin/utils"
+)
+
 type mockDB struct {
 	mockLoadChain func() []byte
 	mockFindBlock func(hash string) []byte
@@ -14,3 +21,26 @@ func (m mockDB) LoadBlockchain() []byte {
 func (mockDB) SaveBlock(hash string, data []byte) {}
 func (mockDB) SaveBlockchain(data []byte)         {}
 func (mockDB) EmptyBlocks()                       {}
+
+func TestBlockchain(t *testing.T) {
+	oldStorage := dbStorage
+	defer func() { dbStorage = oldStorage }()
+	t.Run("Blockchain() should create blockchain when blockchain is nil", func(t *testing.T) {
+		once = *new(sync.Once) // ensure that code in Blockchain() can be run multiple times
+		dbStorage = mockDB{mockLoadChain: func() []byte { return nil }}
+		b := Blockchain()
+		if b.Height != 1 {
+			t.Error("Blockchain() did not create a brand new blockchain")
+		}
+	})
+	t.Run("Blockchain() return existing blockchain when available", func(t *testing.T) {
+		once = *new(sync.Once) // ensure that code in Blockchain() can be run multiple times
+		dbStorage = mockDB{mockLoadChain: func() []byte {
+			return utils.ToBytes(&blockchain{LastHash: "", Height: 2, CurrDifficulty: 1})
+		}}
+		b := Blockchain()
+		if b.Height != 2 {
+			t.Errorf("Expected blockchain of height 2, got height %d", b.Height)
+		}
+	})
+}
