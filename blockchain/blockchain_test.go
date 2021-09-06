3e6ddf3f5b9a662fb9da7e375554861b2a9ffd7a
Andrew Chung <drew.c3071@gmail.com>
2021-09-07 03:41:02 +0900

    coverage for blockchain AddBlockFromPeer and Replace

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -122,10 +122,48 @@ func TestGetDifficulty(t *testing.T) {
 		}
 		for _, tc := range tests {
 			bc := &blockchain{Height: tc.height, CurrDifficulty: defaultDifficulty}
-			got := getDifficulty(bc)
-			if got != tc.expectedOutput {
-				t.Errorf("getDifficulty() should return %d got %d", tc.expectedOutput, got)
+			result := getDifficulty(bc)
+			if result != tc.expectedOutput {
+				t.Errorf("getDifficulty() should return %d got %d", tc.expectedOutput, result)
 			}
+		}
+	})
+}
+
+func TestAddBlockFromPeer(t *testing.T) {
+	oldStorage := dbStorage
+	defer func() { dbStorage = oldStorage }()
+	dbStorage = mockDB{}
+
+	bc := &blockchain{Height: 1, CurrDifficulty: 1, LastHash: "xx"}
+	Mempool().Txs["test"] = &Tx{} // ensure this tx is removed from mempool
+	newBlock := &Block{Difficulty: 2, Hash: "test", Transactions: []*Tx{{Id: "test"}}}
+	bc.AddBlockFromPeer(newBlock)
+
+	t.Run("AddBlockFromPeer() should update the blockchain", func(t *testing.T) {
+		if bc.CurrDifficulty != 2 || bc.Height != 2 || bc.LastHash != "test" {
+			t.Error("AddBlockFromPeer() did not update the blockchain with new block's data")
+		}
+	})
+	t.Run("AddBlockFromPeer() should remove transactions from the mempool", func(t *testing.T) {
+		_, ok := Mempool().Txs["test"]
+		if ok {
+			t.Error("AddBlockFromPeer() should have removed transaction id 'test' from mempool")
+		}
+	})
+
+}
+
+func TestReplace(t *testing.T) {
+	oldStorage := dbStorage
+	defer func() { dbStorage = oldStorage }()
+	dbStorage = mockDB{}
+	t.Run("Replace() should mutate the blockchain", func(t *testing.T) {
+		bc := &blockchain{Height: 1, CurrDifficulty: 1, LastHash: "xx"}
+		blocks := []*Block{{Difficulty: 2, Hash: "test"}, {Difficulty: 2, Hash: "test"}}
+		bc.Replace(blocks)
+		if bc.CurrDifficulty != 2 || bc.Height != 2 || bc.LastHash != "test" {
+			t.Error("Replace() did not update the blockchain with the new blocks")
 		}
 	})
 }
